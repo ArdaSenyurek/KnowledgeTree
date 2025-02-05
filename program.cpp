41d16c33e215a8cd6a5240a271e0884011b3233f
ArdaSenyurek <senyurekardais@gmail.com>
2025-02-05 21:16:36 +0300

    insert/delete tests done

--- program.cpp
+++ program.cpp
@@ -3,13 +3,38 @@
 int main(){
 
   LinkedList<double> ds = {};
-  ds.insertHead(1.3);
-  ds.insertTail(7.6);
+
+  #ifdef DEBUG_INSERT
+  // Insert
+
+  ds.insertTail(1.7);
+  ds.insertHead(1.5);
 
   std::cout << ds;
-  
+  #endif
 
-  std::cout << std::endl;
-  
+  #ifdef DEBUG_DELETE
+  // Delete
+  std::cout << ds;
+  ds.insertHead(1.2);
+  std::cout << ds;
+  ds.insertTail(1.5);
+  std::cout << ds;
+  ds.insertTail(23);
+  std::cout << ds;
+  ds.deleteTail();
+  std::cout << ds;
+  ds.deleteHead();
+  std::cout << ds;
+  ds.deleteNode(1.5);
+  std::cout << ds;
+  #endif
+
+  #ifdef DEBUG_ACC
+  // Access
+  #endif
+
+
+
   return 0;
 }
